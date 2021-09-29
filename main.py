87ecc3e869fc7659916aead52a9579656c241c8d
deekarun123 <81571982+deekarun123@users.noreply.github.com>
2021-09-29 10:57:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,54 +1,23 @@
-import scrapy
-from scrapy.crawler import CrawlerProcess
-import json
-import csv
+from bs4 import BeautifulSoup
+import requests
+from csv import writer
 
+url = "https://www.pararius.com/apartments/amsterdam?ac=1"
+page = requests.get(url)
 
-class Olx(scrapy.Spider):
-    name = 'olx'
+soup = BeautifulSoup(page.content, 'html.parser')
+lists = soup.find_all('section', class_="listing-search-item")
 
-    url = 'https://www.olx.in/api/relevance/search?category=1725&facet_limit=100&location=1000001&location_facet_limit=20&user=1708c08dd02x5e692f19'
+with open('housing.csv', 'w', encoding='utf8', newline='') as f:
+    thewriter = writer(f)
+    header = ['Title', 'Location', 'Price', 'Area']
+    thewriter.writerow(header)
 
-    headers = {
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
-    }
+    for list in lists:
+        title = list.find('a', class_="listing-search-item__link--title").text.replace('\n', '')
+        location = list.find('div', class_="listing-search-item__location").text.replace('\n', '')
+        price = list.find('span', class_="listing-search-item__price").text.replace('\n', '')
+        area = list.find('span', class_="illustrated-features__description").text.replace('\n', '')
 
-    def __init__(self):
-        with open('results.csv', 'w') as csv_file:
-            csv_file.write('title,description,location,features,date,price\n')
-
-    def start_requests(self):
-        for page in range(0, 5):
-            yield scrapy.Request(url=self.url + '&page=' + str(page), headers=self.headers, callback=self.parse)
-
-    def parse(self, res):
-        data = res.text
-        data = json.loads(data)
-
-        for offer in data['data']:
-            items = {
-                'title': offer['title'],
-                'description': offer['description'].replace('\n', ' '),
-                'location': offer['locations_resolved']['COUNTRY_name'] + ', ' +
-                            offer['locations_resolved']['ADMIN_LEVEL_1_name'] + ', ' +
-                            offer['locations_resolved']['ADMIN_LEVEL_3_name'] + ', ' +
-                            offer['locations_resolved']['SUBLOCALITY_LEVEL_1_name'],
-                'features': offer['main_info'],
-                'date': offer['display_date'],
-                'price': offer['price']['value']['display']
-            }
-
-            print(json.dumps(items, indent=2))
-
-            with open('results.csv', 'a') as csv_file:
-                writer = csv.DictWriter(csv_file, fieldnames=items.keys())
-                writer.writerow(items)
-
-
-# run scraper
-process = CrawlerProcess()
-process.crawl(Olx)
-process.start()
-
-# debug
-# Olx.parse(Olx, '')
+        info = [title, location, price, area]
+        thewriter.writerow(info)
